e14693219158d3a4f4c3179517766c4f7cb1843b
PiyushDatta <piyushdattaca@gmail.com>
2019-01-17 01:58:38 -0500

    Okay lets try putting whitenoise runserver on the installed apps

--- src/craiglist_toronto_search/settings.py
+++ src/craiglist_toronto_search/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'corsheaders',
+    # Disable Django's own staticfiles handling in favour of WhiteNoise, for
+    # greater consistency between gunicorn and `./manage.py runserver`. See:
+    # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
