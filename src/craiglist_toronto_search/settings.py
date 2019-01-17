55be82ef0987cd09fb7fc01b9ccee902a6681cc7
PiyushDatta <piyushdattaca@gmail.com>
2019-01-17 02:21:59 -0500

    Just added a line for django extensions

--- src/craiglist_toronto_search/settings.py
+++ src/craiglist_toronto_search/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'craiglist_toronto_search.searchwebapp',
     'crispy_forms',
     'corsheaders',
+    'django_extensions',
     # Disable Django's own staticfiles handling in favour of WhiteNoise, for
     # greater consistency between gunicorn and `./manage.py runserver`. See:
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
